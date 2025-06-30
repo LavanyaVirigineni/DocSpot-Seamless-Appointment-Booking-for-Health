bf93035c2c54c1bd0302c1c2ada8a855a3c92d54
LavanyaVirigineni <viriginenilavanya@gmail.com>
2025-06-30 22:10:38 +0530

    initial commit

---
+++ client/src/redux/api/apiSlice.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8000/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth?.user?.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Doctors"],
+  endpoints: (builder) => ({}),
+});
